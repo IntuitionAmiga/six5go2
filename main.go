ba6c59d8421510fe351c61053d9ab05df0bd36a6
Zayn Otley <intuitionamiga@gmail.com>
2022-09-23 15:56:28 +0200

    Implemented DEY ($88)

--- main.go
+++ main.go
@@ -357,11 +357,38 @@ func execute(file string) {
 			fmt.Printf("TBA\n")
 			incCount(1)
 		case 0x88:
+			/*
+				DEY - Decrement Index Register Y By One
+				Operation: Y - 1 → Y
+
+				This instruction subtracts one from the current value in the index register Y and stores the result
+				into the index register Y. The result does not affect or consider carry so that the value in the index
+				register Y is decremented to 0 and then through 0 to FF.
+
+				Decrement Y does not affect the carry or overflow flags;
+				if the Y register contains bit 7 on as a result of the decrement the N flag is set,
+				otherwise the N flag is reset.
+				If the Y register is 0 as a result of the decrement, the Z flag is set otherwise the Z flag is reset.
+				This instruction only affects the index register Y.
+			*/
 			if printHex {
 				fmt.Printf(";; $%04x\t$%02x\t\t(Implied)\t\n", PC, currentByte())
 			}
 			fmt.Printf("DEY\n")
+
+			//Decrement the  Yregister by 1
+			Y--
+			//If Y register bit 7 is 1, set the SR negative flag bit 7 to 1 else set SR negative flag bit 7 to 0
+			if Y&1<<7 == 1 {
+				//Set bit 7 of SR to 1
+				SR = SR | 1<<7
+			} else {
+				//Set bit 7 of SR to 0
+				SR ^= 1 << 7
+			}
+
 			incCount(1)
+			printMachineState()
 		case 0x8A:
 			if printHex {
 				fmt.Printf(";; $%04x\t$%02x\t\t(Implied)\t\n", PC, currentByte())
