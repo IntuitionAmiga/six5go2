fa01145b6fa85885bcf1cb803e780d2413319b50
Zayn Otley <intuitionamiga@gmail.com>
2022-09-23 23:48:53 +0200

    Implemented SEI ($78)

--- main.go
+++ main.go
@@ -442,11 +442,25 @@ func execute(file string) {
 			fmt.Printf("TZA\n")
 			incCount(1)
 		case 0x78:
+			/*
+				SEI - Set Interrupt Disable
+				Operation: 1 → I
+
+				This instruction initializes the interrupt disable to a 1.
+				It is used to mask interrupt requests during system reset operations and during interrupt commands.
+
+				It affects no registers in the microprocessor and no flags other than the interrupt disable which is set.
+			*/
 			if printHex {
 				fmt.Printf(";; $%04x\t$%02x\t\t(Implied)\t\n", PC, currentByte())
 			}
 			fmt.Printf("SEI\n")
+
+			//Set SR interrupt disable bit 2 to 1
+			SR |= 1 << 2
+
 			incCount(1)
+			printMachineState()
 		case 0x7A:
 			if printHex {
 				fmt.Printf(";; $%04x\t$%02x\t\t(Implied)\t\n", PC, currentByte())
