385bde06dc03e1eb4e1c7fa481c957f980620127
Zayn Otley <intuitionamiga@gmail.com>
2022-09-23 18:55:08 +0200

    Implemented INX ($E8)

--- main.go
+++ main.go
@@ -518,9 +518,12 @@ func execute(file string) {
 				INX - Increment Index Register X By One
 				Operation: X + 1 → X
 
-				Increment X adds 1 to the current value of the X register. This is an 8-bit increment which does not affect the carry operation, therefore, if the value of X before the increment was FF, the resulting value is 00.
+				Increment X adds 1 to the current value of the X register.
+				This is an 8-bit increment which does not affect the carry operation, therefore,
+				if the value of X before the increment was FF, the resulting value is 00.
 
-				INX does not affect the carry or overflow flags; it sets the N flag if the result of the increment has a one in bit 7, otherwise resets N; sets the Z flag if the result of the increment is 0, otherwise it resets the Z flag.
+				INX does not affect the carry or overflow flags; it sets the N flag if the result of the increment
+				has a one in bit 7, otherwise resets N; sets the Z flag if the result of the increment is 0, otherwise it resets the Z flag.
 
 				INX does not affect any other register other than the X register.
 			*/
@@ -529,8 +532,12 @@ func execute(file string) {
 			}
 			fmt.Printf("INX\n")
 
-			//Increment the X register by 1
-			X++
+			//If X==0xFF then X=0 else increment the X by 1
+			if X == 0xFF {
+				X = 0
+			} else {
+				X++
+			}
 
 			incCount(1)
 			printMachineState()
