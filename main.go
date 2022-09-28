5a7ebf1f3b68944f67e0ee9b05ba85a6a96ad001
Zayn Otley <intuitionamiga@gmail.com>
2022-09-28 19:19:36 +0200

    ASL $16

--- main.go
+++ main.go
@@ -1227,10 +1227,43 @@ func execute(file string) {
 			}
 			incCount(2)
 		case 0x16:
+			/*
+				ASL - Arithmetic Shift Left
+				Operation: C ← /M7...M0/ ← 0
+
+				The shift left instruction shifts either the accumulator or the address memory location 1 bit to the
+				left, with the bit 0 always being set to 0 and the the input bit 7 being stored in the carry flag.
+				ASL either shifts the accumulator left 1 bit or is a read/modify/write instruction that affects only memory.
+
+				The instruction does not affect the overflow bit,
+				sets N equal to the result bit 7 (bit 6 in the input),
+				sets Z flag if the result is equal to 0, otherwise resets Z and stores the input bit 7 in the carry flag.
+			*/
 			if printHex {
 				fmt.Printf(";; $%04x\t$%02x $%02x\t\t(ASL - Zero Page,X)\t\n", PC, opcode(), operand1())
 			}
 			fmt.Printf("ASL $%02X,X\n", operand1())
+
+			// Shift left the value at memory[operand1()+X]
+			memory[operand1()+X] <<= 1
+			// If memory[operand1()+X] bit 7 is 1 then set SR carry flag bit 0 to 1 else set SR carry flag bit 0 to 0
+			if memory[operand1()+X]&0b10000000 == 0b10000000 {
+				setSRBitOn(0)
+			} else {
+				setSRBitOff(0)
+			}
+			// If memory[operand1()+X] == 0 then set SR zero flag bit 1 to 1 else set SR zero flag bit 1 to 0
+			if memory[operand1()+X] == 0 {
+				setSRBitOn(1)
+			} else {
+				setSRBitOff(1)
+			}
+			// If memory[operand1()+X] bit 7 is 1 then set SR negative flag bit 7 to 1 else set SR negative flag bit 7 to 0
+			if memory[operand1()+X]&0b10000000 == 0b10000000 {
+				setSRBitOn(7)
+			} else {
+				setSRBitOff(7)
+			}
 			incCount(2)
 		case 0x17:
 			// NOP
