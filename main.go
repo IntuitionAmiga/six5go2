ab207ae977de2850ccff0d8b9aaa8d447507d096
Zayn Otley <intuitionamiga@gmail.com>
2022-10-26 01:54:32 +0200

    Fixed BMI

--- main.go
+++ main.go
@@ -3890,19 +3890,19 @@ func execute() {
 				fmt.Printf("BMI $%02X\n", (bytecounter+2+int(operand1()))&0xFF)
 			}
 
-			// Get offset from relative address in operand
+			// Get offset from operand
 			offset := operand1()
-			// If SR negative flag bit 7 is 1 then branch
+			// If N flag is set, branch to address
 			if getSRBit(7) == 1 {
 				// Branch
 				// Add offset to lower 8bits of PC
-				PC += int(offset)
+				PC = bytecounter + 3 + int(offset)&0xFF
 				// If the offset is negative, decrement the PC by 1
 				// If bit 7 is unset then it's negative
 				if readBit(7, offset) == 0 {
 					PC--
 				}
-				bytecounter += 2
+				bytecounter = PC
 				incCount(0)
 			} else {
 				// Don't branch
