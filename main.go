b97ebeb6fbd43af394c123211c663101d29cfc90
Intuition <intuitionamiga@gmail.com>
2023-09-28 02:27:54 +0200

    Fixed RTS properly

--- main.go
+++ main.go
@@ -120,7 +120,7 @@ func loadROMs() {
 		_, _ = io.ReadFull(file, PLUS4KERNALROM)
 		// Copy the KERNAL ROM data into memory at plus4kernalROMAddress
 		copy(memory[plus4kernalROMAddress:], PLUS4KERNALROM)
-		fmt.Printf("Copying %vKB KERNAL ROM into memory at $%04X to $%04X\n\n", len(PLUS4KERNALROM)/1024, plus4kernalROMAddress, plus4kernalROMAddress+len(PLUS4KERNALROM))
+		fmt.Printf("Copying %vKB KERNAL ROM into memory at $%04X to $%04X\n\n", len(PLUS4KERNALROM)/1024, plus4kernalROMAddress, plus4kernalROMAddress+len(PLUS4KERNALROM)-1)
 		err := file.Close()
 		if err != nil {
 			return
@@ -129,7 +129,7 @@ func loadROMs() {
 		// Open the BASIC ROM file
 		file, _ = os.Open("roms/plus4/basic")
 		_, _ = io.ReadFull(file, PLUS4BASICROM)
-		fmt.Printf("Copying %vKB BASIC ROM into memory at $%04X to $%04X\n\n", len(PLUS4BASICROM)/1024, plus4basicROMAddress, plus4basicROMAddress+len(PLUS4BASICROM))
+		fmt.Printf("Copying %vKB BASIC ROM into memory at $%04X to $%04X\n\n", len(PLUS4BASICROM)/1024, plus4basicROMAddress, plus4basicROMAddress+len(PLUS4BASICROM)-1)
 		copy(memory[plus4basicROMAddress:plus4basicROMAddress+len(PLUS4BASICROM)], PLUS4BASICROM)
 		err = file.Close()
 		if err != nil {
@@ -140,7 +140,7 @@ func loadROMs() {
 		// Load the BASIC ROM
 		file, _ := os.Open("roms/c64/basic")
 		_, _ = io.ReadFull(file, C64BASICROM)
-		fmt.Printf("Copying %vKB BASIC ROM into memory from $%04X to $%04X\n\n", len(C64BASICROM)/1024, c64basicROMAddress, c64basicROMAddress+len(C64BASICROM))
+		fmt.Printf("Copying %vKB BASIC ROM into memory from $%04X to $%04X\n\n", len(C64BASICROM)/1024, c64basicROMAddress, c64basicROMAddress+len(C64BASICROM)-1)
 		copy(memory[c64basicROMAddress:c64basicROMAddress+len(C64BASICROM)], C64BASICROM)
 		err := file.Close()
 		if err != nil {
@@ -150,7 +150,7 @@ func loadROMs() {
 		// Load the KERNAL ROM
 		file, _ = os.Open("roms/c64/kernal")
 		_, _ = io.ReadFull(file, C64KERNALROM)
-		fmt.Printf("Copying %vKB KERNAL ROM into memory from $%04X to $%04X\n\n", len(C64KERNALROM)/1024, c64kernalROMAddress, c64kernalROMAddress+len(C64KERNALROM))
+		fmt.Printf("Copying %vKB KERNAL ROM into memory from $%04X to $%04X\n\n", len(C64KERNALROM)/1024, c64kernalROMAddress, c64kernalROMAddress+len(C64KERNALROM)-1)
 		copy(memory[c64kernalROMAddress:c64kernalROMAddress+len(C64KERNALROM)], C64KERNALROM)
 		err = file.Close()
 		if err != nil {
@@ -160,7 +160,7 @@ func loadROMs() {
 		// Load the CHARACTER ROM
 		file, _ = os.Open("roms/c64/chargen")
 		_, _ = io.ReadFull(file, C64CHARROM)
-		fmt.Printf("Copying %vKB CHARACTER ROM into memory from $%04X to $%04X\n\n", len(C64CHARROM)/1024, c64charROMAddress, c64charROMAddress+len(C64CHARROM))
+		fmt.Printf("Copying %vKB CHARACTER ROM into memory from $%04X to $%04X\n\n", len(C64CHARROM)/1024, c64charROMAddress, c64charROMAddress+len(C64CHARROM)-1)
 		copy(memory[c64charROMAddress:c64charROMAddress+len(C64CHARROM)], C64CHARROM)
 		err = file.Close()
 		if err != nil {
@@ -172,7 +172,7 @@ func loadROMs() {
 		// Copy AllSuiteA ROM into memory
 		file, _ := os.Open("roms/AllSuiteA.bin")
 		_, _ = io.ReadFull(file, AllSuiteAROM)
-		fmt.Printf("Copying %vKB AllSuiteA ROM into memory from $%04X to $%04X\n\n", len(AllSuiteAROM)/1024, AllSuiteAROMAddress, AllSuiteAROMAddress+len(AllSuiteAROM))
+		fmt.Printf("Copying %vKB AllSuiteA ROM into memory from $%04X to $%04X\n\n", len(AllSuiteAROM)/1024, AllSuiteAROMAddress, AllSuiteAROMAddress+len(AllSuiteAROM)-1)
 		copy(memory[AllSuiteAROMAddress:], AllSuiteAROM)
 		// Set the interrupt vector addresses manually
 		writeMemory(IRQVectorAddress, 0x00)   // Low byte of 0x4000
@@ -188,7 +188,7 @@ func loadROMs() {
 		file, _ := os.Open("roms/6502_functional_test.bin")
 		_, _ = io.ReadFull(file, KlausDTestROM)
 		copy(memory[KlausDTestROMAddress:], KlausDTestROM)
-		fmt.Printf("Copying Klaus Dormann's %vKB 6502 functional test ROM into memory from $%04X to $%04X\n\n", len(KlausDTestROM)/1024, KlausDTestROMAddress, KlausDTestROMAddress+len(KlausDTestROM))
+		fmt.Printf("Copying Klaus Dormann's %vKB 6502 functional test ROM into memory from $%04X to $%04X\n\n", len(KlausDTestROM)/1024, KlausDTestROMAddress, KlausDTestROMAddress+len(KlausDTestROM)-1)
 		// Set the interrupt vector addresses manually
 		writeMemory(IRQVectorAddress, 0x00)   // Low byte of 0x4000
 		writeMemory(IRQVectorAddress+1, 0x40) // High byte of 0x4000
@@ -203,7 +203,7 @@ func loadROMs() {
 		file, _ := os.Open("roms/TTL6502.BIN")
 		_, _ = io.ReadFull(file, RuudBTestROM)
 		copy(memory[RuudBTestROMAddress:], RuudBTestROM)
-		fmt.Printf("Copying Ruud B's %vKB Test ROM into memory from $%04X to $%04X\n\n", len(RuudBTestROM)/1024, RuudBTestROMAddress, RuudBTestROMAddress+len(RuudBTestROM))
+		fmt.Printf("Copying Ruud B's %vKB Test ROM into memory from $%04X to $%04X\n\n", len(RuudBTestROM)/1024, RuudBTestROMAddress, RuudBTestROMAddress+len(RuudBTestROM)-1)
 		err := file.Close()
 		if err != nil {
 			return
@@ -2307,18 +2307,17 @@ func execute() {
 			*/
 			disassembledInstruction = fmt.Sprintf("RTS")
 			disassembleOpcode()
-			//Get low byte of PC
+			//Get low byte of new PC
 			low := uint16(readStack())
-			// Increment the stack pointer by 1 byte
+			// Increment the stack pointer
 			incSP()
-			//Get high byte of PC
-			//incSP()
+			//Get high byte of new PC
 			high := uint16(readStack())
 			previousPC = PC
 			previousOpcode = opcode()
 			//Update PC with the value stored in memory at the address pointed to by SP
+			setPC(int((high << 8) | low))
 			handleState(0)
-			setPC(int((high<<8)|low) + 1)
 		case 0x38:
 			/*
 				SEC - Set Carry Flag
